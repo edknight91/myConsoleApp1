35760b950e5fa450b6b8af05877f0baa309bf22e
edknight91 <ed.knight1991@gmail.com>
2021-10-14 20:31:08 +0100

    first program effort

--- Program.cs
+++ Program.cs
@@ -1,12 +1,28 @@
 ﻿using System;
-
+using System.IO;
 namespace myConsoleApp1
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("What's your name?");
+            string userName = Console.ReadLine();
+            Console.WriteLine($"Hello, {userName}");
+            Console.WriteLine("Are you having a good day? Enter Y/N");
+
+            string input = Console.ReadKey().Key.ToString();
+            char response = Convert.ToChar(input);
+
+
+            if (response == 'Y')
+            {
+                Console.WriteLine($"Good shit {userName}");
+            }
+            if (response == 'N')
+            {
+                Console.WriteLine("Ah balls");
+            }
         }
     }
 }
